35ebca8c42e8e8ee3397b47b4f527371195a7777
SaiVeer04 <43910229+SaiVeer04@users.noreply.github.com>
2018-10-20 19:34:15 -0400

    TeleOp by Sai

---
+++ TeleOp.java
@@ -0,0 +1,141 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOpIntake")
+public class TeleOp extends LinearOpMode {
+    public DcMotor BR;
+    public DcMotor BL;
+    public DcMotor FR;
+    public DcMotor FL;
+    public DcMotor intake;
+    public DcMotor rotateIntake;
+    public DcMotor DrawerIntake;
+    public Servo   BasketServo;
+    public DcMotor DrawerOutake;
+
+
+   int intakevalue = 3200;
+   int rotateIntake2 = 540;
+   int glyphServoCounter = 1;
+
+
+
+
+
+    public void runOpMode ()throws InterruptedException{
+        BR = hardwareMap.get(DcMotor.class, "rightRear");
+
+        BL = hardwareMap.get(DcMotor.class, "leftRear");
+
+        FR = hardwareMap.get(DcMotor.class, "rightFront");
+
+        FL = hardwareMap.get(DcMotor.class, "leftFront");
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        rotateIntake = hardwareMap.get(DcMotor.class, "rotateIntake");
+
+        DrawerIntake = hardwareMap.dcMotor.get("DI");
+
+        DrawerOutake = hardwareMap.dcMotor.get("hangMotor");
+
+        BL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+
+
+            waitForStart();
+            if(opModeIsActive()) {
+                while(opModeIsActive()){
+                    double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+                    double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+                    double rightX = gamepad1.right_stick_x;
+                    final double v1 = r * Math.cos(robotAngle) + rightX;
+                    final double v2 = r * Math.sin(robotAngle) - rightX;
+                    final double v3 = r * Math.sin(robotAngle) + rightX;
+                    final double v4 = r * Math.cos(robotAngle) - rightX;
+
+                    FL.setPower(v1);
+                    FR.setPower(v2);
+                    BL.setPower(v3);
+                    BR.setPower(v4);
+
+                    FL.setPower(v1);
+                    BL.setPower(v3);
+                    FR.setPower(v2);
+                    BR.setPower(v4);
+
+                    rotateIntake.setPower(gamepad1.left_trigger);
+
+                    rotateIntake.setPower(-gamepad1.right_trigger);
+
+                  /*  if(gamepad1.b){
+                        BasketServo.setPosition(0.5);
+                    } else if(gamepad1.x){
+                        BasketServo.setPosition(0);
+                    } */
+                    if (gamepad1.right_bumper) {
+                        intake.setTargetPosition(intakevalue);
+                        encoderIntake(0.3);
+                    }
+                    if (gamepad1.left_bumper) {
+                        intake.setTargetPosition(-intakevalue);
+                        encoderIntake(0.3);
+                    }
+
+
+                    if(gamepad1.dpad_up){
+                        DrawerIntake.setPower(-.3);
+                        Thread.sleep(1000);
+                    }
+                    if (gamepad1.dpad_down){
+                        DrawerIntake.setPower(.3);
+                        Thread.sleep(1000);
+                    }
+
+                    if (gamepad1.a){
+
+                        DrawerOutake.setPower(0.3);
+                        Thread.sleep(1000);
+
+                    }
+                    if (gamepad1.y){
+
+                        DrawerOutake.setPower(-0.3);
+                        Thread.sleep(1000);
+
+                    }
+
+
+
+                }
+
+
+
+
+
+
+            }
+    }
+    public void encoderIntake(double setPower){
+        if(opModeIsActive()){
+            intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            intake.setPower(setPower);
+
+            intake.setPower(0);
+
+            intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+
+    }
+
+}
