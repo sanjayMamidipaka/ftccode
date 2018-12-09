15b3fb028dff1f3b223ecb04b34d8ef5bf52a67f
aghayalod <43766978+aghayalod@users.noreply.github.com>
2018-12-08 20:08:37 -0500

    Add files via upload

--- TeleOpSai.java
+++ TeleOpSai.java
@@ -1,24 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOpSai")
 public class TeleOpSai extends LinearOpMode {
+    //DRIVE TRAIN
     public DcMotor BR;
     public DcMotor BL;
     public DcMotor FR;
     public DcMotor FL;
-    public DcMotor intake;
-    public DcMotor rotateIntake;
-    public DcMotor DrawerIntake;
-    public Servo   BasketServo;
-    public DcMotor hangLift;
-    public Servo mineralServo;
 
-    int servoCounter;
+    public CRServo intake;
+
+    public DcMotor arm;
+
+    public DcMotor hook1;
+    public DcMotor hook2;
+
+
 
 
 
@@ -27,6 +30,7 @@ public class TeleOpSai extends LinearOpMode {
 
 
     public void runOpMode ()throws InterruptedException{
+       /*
         BR = hardwareMap.get(DcMotor.class, "rightRear");
 
         BL = hardwareMap.get(DcMotor.class, "leftRear");
@@ -35,21 +39,19 @@ public class TeleOpSai extends LinearOpMode {
 
         FL = hardwareMap.get(DcMotor.class, "leftFront");
 
-        intake = hardwareMap.get(DcMotor.class, "intake");
+        intake = hardwareMap.get(CRServo.class, "intake");
 
-        rotateIntake = hardwareMap.get(DcMotor.class, "rotateIntake");
-
-        DrawerIntake = hardwareMap.dcMotor.get("DI");
-
-        hangLift = hardwareMap.dcMotor.get("hangMotor");
-
-        mineralServo = hardwareMap.servo.get("mineralServo");
-
-        BasketServo = hardwareMap.servo.get("bk");
+        arm = hardwareMap.get(DcMotor.class, "arm" );
+        */
 
 
-        BL.setDirection(DcMotorSimple.Direction.REVERSE);
-        FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        hook1 = hardwareMap.get(DcMotor.class, "hangingMotor");
+        hook2 = hardwareMap.get(DcMotor.class, "hangingMotor1");
+
+
+
+       // BL.setDirection(DcMotorSimple.Direction.REVERSE);
+        //FL.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
 
@@ -58,6 +60,7 @@ public class TeleOpSai extends LinearOpMode {
         waitForStart();
         if(opModeIsActive()) {
             while(opModeIsActive()){
+                /*
                 double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
                 double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
                 double rightX = (-gamepad1.right_stick_x)/2.0;
@@ -73,80 +76,55 @@ public class TeleOpSai extends LinearOpMode {
                 BL.setPower(v3);
                 BR.setPower(v4);
 
+                //COLLECTION
+               if (gamepad1.right_bumper)
+               {
+                   intake.setPower(0.2);
+               }
+               else
+               {
+                   intake.setPower(0);
+               }
 
-                rotateIntake.setPower(gamepad1.left_trigger);
+               if(gamepad1.left_bumper)
+               {
+                   intake.setPower(-0.2);
+               }
+               else
+               {
+                   intake.setPower(0);
+               }
 
-                rotateIntake.setPower(-gamepad1.right_trigger);
+               //ARM
+               arm.setPower(gamepad1.left_trigger);
+               arm.setPower(-gamepad1.right_trigger);
+            */
+               //LIFT
 
-                    if(gamepad1.b){
-                        BasketServo.setPosition(0.8);
-                    } else if(gamepad1.x){
-                        BasketServo.setPosition(0.2);
-                    }
-                if (gamepad1.right_bumper) {
-                    intake.setPower(1);
+                if(gamepad1.y)
+                {
+                    hook1.setPower(0.3);
+                    hook2.setPower(-0.3);
                 }
                 else
                 {
-                    intake.setPower(0);
+                    hook2.setPower(0);
+                    hook1.setPower(0);
                 }
 
-                /*while (isLeftDown)
+                if(gamepad1.a)
                 {
-                    intake.setTargetPosition(-intakevalue);
-                    if(!isLeftDown)
-                    {
-                        break;
-                    }
-                }*/
-                if (gamepad1.left_bumper)
-                {
-                    intake.setPower(-1);
+                    hook1.setPower(-0.3);
+                    hook2.setPower(0.3);
                 }
                 else
                 {
-                    intake.setPower(0);
+                    hook2.setPower(0);
+                    hook1.setPower(0);
                 }
 
 
-                if(gamepad1.dpad_up){
-                    DrawerIntake.setPower(-1);
-                }
-                else{
-                    DrawerIntake.setPower(0);
-                }
-                if (gamepad1.dpad_down){
-                    DrawerIntake.setPower(.3);
-                }
-                else {
-                    DrawerIntake.setPower(0);
-                }
 
-                if (gamepad1.a){
-
-                    hangLift.setPower(0.3);
-
-                }else{
-                    hangLift.setPower(0);
-                }
-                if (gamepad1.y){
-
-                    hangLift.setPower(-0.3);
-
-                }else{
-                    hangLift.setPower(0);
-                }
-
-                if(gamepad1.x && servoCounter%2 == 0)
-                {
-                    mineralServo.setPosition(0.5);
-                    servoCounter++;
-                }
-                else if (servoCounter%2 != 0 )
-                {
-                    mineralServo.setPosition(0);
-                    servoCounter++;
-                }
 
 
 
@@ -160,18 +138,6 @@ public class TeleOpSai extends LinearOpMode {
 
         }
     }
-    public void encoderIntake(double setPower){
-        if(opModeIsActive()){
-            intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            intake.setPower(setPower);
-
-            intake.setPower(0);
-
-            intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        }
-
-    }
 
 }
