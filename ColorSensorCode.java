d671c33318db5ff46c2fdeb7ce1dbfe6d7d33fea
sanjayMamidipaka <43594347+sanjayMamidipaka@users.noreply.github.com>
2018-12-09 14:23:05 -0500

    Add files via upload

--- ColorSensorCode.java
+++ ColorSensorCode.java
@@ -8,11 +8,14 @@ import android.view.View;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.robot.Robot;
+//import com.qualcomm.robotcore.Robot;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwareConfig;
+//import org.firstinspires.ftc.robotcontroller.external.samples.HardwareConfig;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -21,46 +24,90 @@ import java.util.Locale;
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "RoverAutonmous")
 public class ColorSensorCode extends LinearOpMode {
 
-    ColorSensor sensorColor;
+    public ColorSensorCode() {
+
+    }
 
     //DistanceSensor sensorDistance;
-    DcMotor rightRear;
-    hardwareMapTest robot = new hardwareMapTest();
 
+    /*static final double COUNTS_PER_MOTOR_REV = 1220;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 025;*/
+
+    public DcMotor rightRear;
+    public DcMotor leftRear;
+    public DcMotor rightFront;
+    public DcMotor leftFront;
+    public ColorSensor sensorColor;
+    public DcMotor hangMotor;
+
+    //from TeleOp
+
+
+    private ElapsedTime runtime = new ElapsedTime();
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-    robot.init(hardwareMap);
-
-        // get a reference to the color sensor.
-        sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
-        rightRear = hardwareMap.dcMotor.get("rightRear");
-
-        // get a reference to the distance sensor that shares the same name.
-        //sensorDistance = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
-
-        // hsvValues is an array that will hold the hue, saturation, and value information.
-        float hsvValues[] = {0F, 0F, 0F};
-
-        // values is a reference to the hsvValues array.
-        final float values[] = hsvValues;
-
-        // sometimes it helps to multiply the raw RGB values with a scale factor
-        // to amplify/attentuate the measured values.
-        final double SCALE_FACTOR = 255;
-
-        // get a reference to the RelativeLayout so we can change the background
-        // color of the Robot Controller app to match the hue detected by the RGB sensor.
-        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
-        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
-
-        // wait for the start button to be pressed.
         waitForStart();
 
-        // loop and read the RGB and distance data.
-        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
-        while (opModeIsActive()) {
+        //init(hardwareMap);
+        //first, the robot will have to land onto the playing field, no code for that yet
+
+        boolean isYellow = false;
+        boolean isRight = false;
+        double power = 0.2;
+        rightRear = hardwareMap.dcMotor.get("rightRear");
+        leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        sensorColor = hardwareMap.get(ColorSensor.class, "sensorColor");
+
+
+        rightFront.setPower(0.5);
+        rightRear.setPower(0.5);
+        leftFront.setPower(-0.5);
+        leftRear.setPower(-0.5);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.milliseconds() < 700)) {
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.milliseconds());
+            telemetry.update();
+        }
+        rightFront.setPower(0);
+        rightRear.setPower(0);
+        leftFront.setPower(0);
+        leftRear.setPower(0);
+        sleep(2000);
+        telemetry.addData("Not going yet", 0);
+
+
+        while (isYellow == false) {
+
+            // get a reference to the distance sensor that shares the same name.
+            //sensorDistance = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
+
+            // hsvValues is an array that will hold the hue, saturation, and value information.
+            float hsvValues[] = {0F, 0F, 0F};
+
+            // values is a reference to the hsvValues array.
+            final float values[] = hsvValues;
+
+            // sometimes it helps to multiply the raw RGB values with a scale factor
+            // to amplify/attentuate the measured values.
+            final double SCALE_FACTOR = 255;
+
+            // get a reference to the RelativeLayout so we can change the background
+            // color of the Robot Controller app to match the hue detected by the RGB sensor.
+            int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+            final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+            // wait for the start button to be pressed.
+            // loop and read the RGB and distance data.
+            // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
             // convert the RGB values to HSV values.
             // multiply by the SCALE_FACTOR.
             // then cast it back to int (SCALE_FACTOR is a double)
@@ -72,46 +119,142 @@ public class ColorSensorCode extends LinearOpMode {
             // send the info back to driver station using telemetry function.
             //telemetry.addData("Distance (cm)",
             //String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
-            boolean isYellow = true;
-            while (isYellow) { //constantly check for change
-                if (sensorColor.red() > sensorColor.green()) { //since yellow has a higher red value than green
-                    telemetry.addData("Yellow Detected: True", 1);
-                    isYellow = false; //gets you out of the for loop
-                    //encoderDrive(0.2, 2, 2, 2, 2, 1); //method for moving, in Hardware.java
-                    robot.run();
 
-                } else {
-                    telemetry.addData("Yellow Detected: False", 1);
-                    //isYellow = false;
+
+            if (sensorColor.red() > sensorColor.green()) { //since yellow has a higher red value than green
+                telemetry.addData("Yellow Detected: True", 1);
+                //movement(0.2, 100, -1, -1, -1, -1);
+                rightFront.setPower(0.5);
+                rightRear.setPower(0.5);
+                leftFront.setPower(-0.5);
+                leftRear.setPower(-0.5);
+                runtime.reset();
+                while (opModeIsActive() && (runtime.milliseconds() < 1700)) {
+                    telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.milliseconds());
+                    telemetry.update();
                 }
+                rightFront.setPower(0);
+                rightRear.setPower(0);
+                leftFront.setPower(0);
+                leftRear.setPower(0);
+                sleep(5000);
+                isYellow = true;
+
+            } else {
+
+                telemetry.addData("Yellow Detected: False", 1);
+                //movement(0.2, 100, 1, -1, -1, 1);
+
+
+                telemetry.addData("Alpha", sensorColor.alpha());
+                telemetry.addData("Red  ", sensorColor.red());
+                telemetry.addData("Green", sensorColor.green());
+                telemetry.addData("Blue ", sensorColor.blue());
+                telemetry.addData("Hue", hsvValues[0]);
+
+                rightFront.setPower(0.3); //strafes left
+                rightRear.setPower(-0.3);
+                leftFront.setPower(0.3);
+                leftRear.setPower(-0.3);
+                runtime.reset();
+                while (opModeIsActive() && (runtime.milliseconds() < 2000)) {
+                    telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.milliseconds());
+                    telemetry.update();
+                }
+                rightFront.setPower(0);
+                rightRear.setPower(0);
+                leftFront.setPower(0);
+                leftRear.setPower(0);
+                sleep(2000);
+                runtime.reset();
+
+                if (sensorColor.red() > sensorColor.green()) // checks if the sample is yellow
+                {
+                    telemetry.addData("Yellow: True", 0);
+                    sleep(1000);
+                    runtime.reset();
+                    rightFront.setPower(0.5); //moves forward
+                    rightRear.setPower(0.5);
+                    leftFront.setPower(-0.5);
+                    leftRear.setPower(-0.5);
+                    runtime.reset();
+                    while (opModeIsActive() && (runtime.milliseconds() < 1700)) {
+                        telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.milliseconds());
+                        telemetry.update();
+                    }
+                    rightFront.setPower(0);
+                    rightRear.setPower(0);
+                    leftFront.setPower(0);
+                    leftRear.setPower(0);
+                }
+                else //if there is white
+                {
+                    telemetry.addData("Yellow: False", 0);
+                    sleep(1000);
+                    runtime.reset();
+
+                    sleep(2000);
+
+                    runtime.reset();
+                    rightFront.setPower(0.3); //strafe right
+                    rightRear.setPower(-0.3);
+                    leftFront.setPower(0.3);
+                    leftRear.setPower(-0.3);
+                    runtime.reset();
+                    while (opModeIsActive() && (runtime.milliseconds() < 2400)) {
+                        telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.milliseconds());
+                        telemetry.update();
+                    }
+                    rightFront.setPower(0);
+                    rightRear.setPower(0);
+                    leftFront.setPower(0);
+                    leftRear.setPower(0);
+                }
+
+
+
+                // change the background color to match the color detected by the RGB sensor.
+                // pass a reference to the hue, saturation, and value array as an argument
+                // to the HSVToColor method.
+                relativeLayout.post(new Runnable() {
+                    public void run() {
+                        relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                    }
+                });
+
+                telemetry.update();
+                stop();
             }
-            telemetry.addData("Alpha", sensorColor.alpha());
-            telemetry.addData("Red  ", sensorColor.red());
-            telemetry.addData("Green", sensorColor.green());
-            telemetry.addData("Blue ", sensorColor.blue());
-            telemetry.addData("Hue", hsvValues[0]);
 
-            // change the background color to match the color detected by the RGB sensor.
-            // pass a reference to the hue, saturation, and value array as an argument
-            // to the HSVToColor method.
-            relativeLayout.post(new Runnable() {
-                public void run() {
-                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
-                }
-            });
-
-            telemetry.update();
         }
-
-        // Set the panel back to the default color
-        relativeLayout.post(new Runnable() {
-            public void run() {
-                relativeLayout.setBackgroundColor(Color.WHITE);
-            }
-        });
 
     }
 
 
-}
+        public void movement ( double LFPower, double LRPower, double RFPower, double RRPower,
+        double runSecs)
+        {
+
+
+            rightFront.setPower(RFPower); //strafes right
+            rightRear.setPower(RRPower);
+            leftFront.setPower(LFPower);
+            leftRear.setPower(LRPower);
+            runtime.reset();
+            while (opModeIsActive() && (runtime.milliseconds() < runSecs)) {
+                telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.milliseconds());
+                telemetry.update();
+            }
+            runtime.reset();
+            rightFront.setPower(0);
+            rightRear.setPower(0);
+            leftFront.setPower(0);
+            leftRear.setPower(0);
+            }
+
+
+
+        }
+
+
 
